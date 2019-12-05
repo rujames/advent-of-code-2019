ecbada57c10d27a962f30a70c003ac45d8136730
Ru James <ruairidh.d.james@gmail.com>
2019-12-05 20:33:36 +0000

    Day 5 - Part 2!

--- src/advent_of_code_2019/day5.clj
+++ src/advent_of_code_2019/day5.clj
@@ -122,3 +122,127 @@ After providing 1 to the only input instruction and passing all the tests, what 
        vec))
 
 (run loaded-prog)
+
+"""
+--- Part Two ---
+The air conditioner comes online! Its cold air feels good for a while, but then the TEST alarms start to go off. Since the air conditioner can't vent its heat anywhere but back into the spacecraft, it's actually making the air inside the ship warmer.
+
+Instead, you'll need to use the TEST to extend the thermal radiators. Fortunately, the diagnostic program (your puzzle input) is already equipped for this. Unfortunately, your Intcode computer is not.
+
+Your computer is only missing a few opcodes:
+
+Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+Like all instructions, these instructions need to support parameter modes as described above.
+
+Normally, after an instruction is finished, the instruction pointer increases by the number of values in that instruction. However, if the instruction modifies the instruction pointer, that value is used and the instruction pointer is not automatically increased.
+
+For example, here are several programs that take one input, compare it to the value 8, and then produce one output:
+
+3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
+
+3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 (using position mode)
+3,3,1105,-1,9,1101,0,0,12,4,12,99,1 (using immediate mode)
+Here's a larger example:
+
+3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+The above example program uses an input instruction to ask for a single number. The program will then output 999 if the input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is greater than 8.
+
+This time, when the TEST diagnostic program runs its input instruction to get the ID of the system to test, provide it 5, the ID for the ship's thermal radiator controller. This diagnostic test suite only outputs one number, the diagnostic code.
+
+What is the diagnostic code for system ID 5?
+"""
+
+
+(defn input [output]
+  (println "input" output)
+  (fn [program pointer out]
+    [(assoc program output 5) (+ pointer 2) out]))
+
+(defn jump-if-true [p p-mode x x-mode]
+  (println "jump-if-true" p p-mode x x-mode)
+  (let [p' (if (= p-mode 0) #(nth % p) (constantly p))
+        x' (if (= x-mode 0) #(nth % x) (constantly x))]
+    (fn [program pointer out]
+      (if (> (p' program) 0)
+        [program (x' program) out]
+        [program (+ pointer 3) out]))))
+
+(defn jump-if-false [p p-mode x x-mode]
+  (println "jump-if-false" p p-mode x x-mode)
+  (let [p' (if (= p-mode 0) #(nth % p) (constantly p))
+        x' (if (= x-mode 0) #(nth % x) (constantly x))]
+    (fn [program pointer out]
+      (if (= (p' program) 0)
+        [program (x' program) out]
+        [program (+ pointer 3) out]))))
+
+(defn less-than [x x-mode y y-mode output]
+  (println "less-than" x x-mode y y-mode output)
+  (let [x' (if (= x-mode 0) #(nth % x) (constantly x))
+        y' (if (= y-mode 0) #(nth % y) (constantly y))]
+    (fn [program pointer out]
+      (if (< (x' program) (y' program))
+        [(assoc program output 1) (+ pointer 4) out]
+        [(assoc program output 0) (+ pointer 4) out]))))
+
+(defn equals [x x-mode y y-mode output]
+  (println "equals" x x-mode y y-mode output)
+  (let [x' (if (= x-mode 0) #(nth % x) (constantly x))
+        y' (if (= y-mode 0) #(nth % y) (constantly y))]
+    (fn [program pointer out]
+      (if (= (x' program) (y' program))
+        [(assoc program output 1) (+ pointer 4) out]
+        [(assoc program output 0) (+ pointer 4) out]))))
+
+(defn step [program pointer out]
+  (let [instruction (nth program pointer)
+        opcode (mod instruction 100)
+        modes' (modes instruction)]
+    ((condp = (mod (nth program pointer) 100)
+       1 (add
+          (nth program (+ pointer 1))
+          (first modes')
+          (nth program (+ pointer 2))
+          (second modes')
+          (nth program (+ pointer 3)))
+       2 (multiply
+          (nth program (+ pointer 1))
+          (first modes')
+          (nth program (+ pointer 2))
+          (second modes')
+          (nth program (+ pointer 3)))
+       3 (input (nth program (+ pointer 1)))
+       4 (output (nth program (+ pointer 1)))
+       5 (jump-if-true
+          (nth program (+ pointer 1))
+          (first modes')
+          (nth program (+ pointer 2))
+          (second modes'))
+       6 (jump-if-false
+          (nth program (+ pointer 1))
+          (first modes')
+          (nth program (+ pointer 2))
+          (second modes'))
+       7 (less-than
+          (nth program (+ pointer 1))
+          (first modes')
+          (nth program (+ pointer 2))
+          (second modes')
+          (nth program (+ pointer 3)))
+       8 (equals
+          (nth program (+ pointer 1))
+          (first modes')
+          (nth program (+ pointer 2))
+          (second modes')
+          (nth program (+ pointer 3)))
+       99 (terminate))
+     program pointer out)))
