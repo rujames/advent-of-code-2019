c39d26a0e8bc686bc4ebee1bf8c6fd97fb1533c7
Ru James <ruairidh.d.james@gmail.com>
2019-12-07 11:41:03 +0000

    Day 7 - Part 2!

--- src/advent_of_code_2019/day7.clj
+++ src/advent_of_code_2019/day7.clj
@@ -61,3 +61,67 @@ Try every combination of phase settings on the amplifiers. What is the highest s
       (cons head tail))))
 
 (apply max (map trial (permutations #{0 1 2 3 4})))
+
+"""
+--- Part Two ---
+It's no good - in this configuration, the amplifiers can't generate a large enough output signal to produce the thrust you'll need. The Elves quickly talk you through rewiring the amplifiers into a feedback loop:
+
+      O-------O  O-------O  O-------O  O-------O  O-------O
+0 -+->| Amp A |->| Amp B |->| Amp C |->| Amp D |->| Amp E |-.
+   |  O-------O  O-------O  O-------O  O-------O  O-------O |
+   |                                                        |
+   '--------------------------------------------------------+
+                                                            |
+                                                            v
+                                                     (to thrusters)
+Most of the amplifiers are connected as they were before; amplifier A's output is connected to amplifier B's input, and so on. However, the output from amplifier E is now connected into amplifier A's input. This creates the feedback loop: the signal will be sent through the amplifiers many times.
+
+In feedback loop mode, the amplifiers need totally different phase settings: integers from 5 to 9, again each used exactly once. These settings will cause the Amplifier Controller Software to repeatedly take input and produce output many times before halting. Provide each amplifier its phase setting at its first input instruction; all further input/output instructions are for signals.
+
+Don't restart the Amplifier Controller Software on any amplifier during this process. Each one should continue receiving and sending signals until it halts.
+
+All signals sent or received in this process will be between pairs of amplifiers except the very first signal and the very last signal. To start the process, a 0 signal is sent to amplifier A's input exactly once.
+
+Eventually, the software on the amplifiers will halt after they have processed the final loop. When this happens, the last output signal from amplifier E is sent to the thrusters. Your job is to find the largest output signal that can be sent to the thrusters using the new phase settings and feedback loop arrangement.
+
+Here are some example programs:
+
+Max thruster signal 139629729 (from phase setting sequence 9,8,7,6,5):
+
+3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5
+Max thruster signal 18216 (from phase setting sequence 9,7,8,5,6):
+
+3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10
+Try every combination of the new phase settings on the amplifier feedback loop. What is the highest signal that can be sent to the thrusters?
+"""
+
+(defn run-until-output [initial-state inputs]
+  (loop [memory (first initial-state)
+         pointer (second initial-state)
+         in inputs
+         out []]
+    (let [next-state (intcode/step memory pointer in out)]
+      (if (nil? next-state) (last inputs)
+          (if (int? next-state) next-state
+              (if (empty? (nth next-state 3))
+                (recur (nth next-state 0) (nth next-state 1) (nth next-state 2) (nth next-state 3))
+                next-state))))))
+
+(defn default-initial-state [phase]
+  [program 0 [phase] []])
+
+(defn feedback-trial [settings]
+  (loop [states (vec (map default-initial-state settings))
+         current 0
+         input 0]
+    (let [next-state (run-until-output (nth states current) (conj (nth (nth states current) 2) input))]
+      (if (int? next-state)
+        (if (= current 4)
+          next-state
+          (last (nth (nth states 4) 3)))
+        (recur (assoc states current next-state) (mod (inc current) 5) (first (nth next-state 3)))))))
+
+(apply max (map feedback-trial (permutations #{5 6 7 8 9})))
