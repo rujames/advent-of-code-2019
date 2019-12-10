07eef4f9f3f4ce11ab679fa0a197653b80907dd9
Ru James <ruairidh.d.james@gmail.com>
2019-12-10 21:10:04 +0000

    Day 10 - Part 1!

---
+++ src/advent_of_code_2019/day10.clj
@@ -0,0 +1,126 @@
+(ns advent-of-code-2019.day10
+  (:require [clojure.string :as str]))
+
+"""
+--- Day 10: Monitoring Station ---
+You fly into the asteroid belt and reach the Ceres monitoring station. The Elves here have an emergency: they're having trouble tracking all of the asteroids and can't be sure they're safe.
+
+The Elves would like to build a new monitoring station in a nearby area of space; they hand you a map of all of the asteroids in that region (your puzzle input).
+
+The map indicates whether each position is empty (.) or contains an asteroid (#). The asteroids are much smaller than they appear on the map, and every asteroid is exactly in the center of its marked position. The asteroids can be described with X,Y coordinates where X is the distance from the left edge and Y is the distance from the top edge (so the top-left corner is 0,0 and the position immediately to its right is 1,0).
+
+Your job is to figure out which asteroid would be the best place to build a new monitoring station. A monitoring station can detect any asteroid to which it has direct line of sight - that is, there cannot be another asteroid exactly between them. This line of sight can be at any angle, not just lines aligned to the grid or diagonally. The best location is the asteroid that can detect the largest number of other asteroids.
+
+For example, consider the following map:
+
+.#..#
+.....
+#####
+....#
+...##
+The best location for a new monitoring station on this map is the highlighted asteroid at 3,4 because it can detect 8 asteroids, more than any other location. (The only asteroid it cannot detect is the one at 1,0; its view of this asteroid is blocked by the asteroid at 2,2.) All other asteroids are worse locations; they can detect 7 or fewer other asteroids. Here is the number of other asteroids a monitoring station on each asteroid could detect:
+
+.7..7
+.....
+67775
+....7
+...87
+Here is an asteroid (#) and some examples of the ways its line of sight might be blocked. If there were another asteroid at the location of a capital letter, the locations marked with the corresponding lowercase letter would be blocked and could not be detected:
+
+#.........
+...A......
+...B..a...
+.EDCG....a
+..F.c.b...
+.....c....
+..efd.c.gb
+.......c..
+....f...c.
+...e..d..c
+Here are some larger examples:
+
+Best is 5,8 with 33 other asteroids detected:
+
+......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####
+Best is 1,2 with 35 other asteroids detected:
+
+#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.
+Best is 6,3 with 41 other asteroids detected:
+
+.#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..
+Best is 11,13 with 210 other asteroids detected:
+
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+Find the best location for a new monitoring station. How many other asteroids can be detected from that location?
+"""
+
+(defn signed-gradient [station asteroid]
+  (let [dx (- (first station) (first asteroid))
+        dy (- (second station) (second asteroid))]
+    (if (not= dx 0)
+      [(> dx 0) (/ dy dx)]
+      (if (not= dy 0)
+        [(> dy 0) :infinity]))))        
+
+(defn detectable-asteroids [station asteroids]
+  (count (distinct (keep (partial signed-gradient station) asteroids))))
+
+(defn best-station [asteroids]
+  (apply max-key #(detectable-asteroids % asteroids) asteroids))
+
+(defn read-line [line]
+  (keep-indexed (fn [x pt] (if (= pt \#) x)) line))
+
+(defn read-lines [lines]
+  (apply concat (map-indexed (fn [y line] (map #(vector % y) (read-line line))) lines)))
+
+(def input (read-lines (str/split (slurp "src/advent_of_code_2019/day10.input") #"\r\n")))
+
+;; (detectable-asteroids (best-station input) input)
+;; => 269
