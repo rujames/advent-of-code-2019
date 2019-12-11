5d7cefa572e5b212b982e8a8105f6cbfe06a1503
Ru James <ruairidh.d.james@gmail.com>
2019-12-11 19:02:38 +0000

    Day 10 - Part 2!

--- src/advent_of_code_2019/day10.clj
+++ src/advent_of_code_2019/day10.clj
@@ -100,13 +100,14 @@ Best is 11,13 with 210 other asteroids detected:
 Find the best location for a new monitoring station. How many other asteroids can be detected from that location?
 """
 
+;; Signed by clockwise half-turn: [North, South) => true, [South, North) => false
 (defn signed-gradient [station asteroid]
-  (let [dx (- (first station) (first asteroid))
+  (let [dx (- (first asteroid) (first station))
         dy (- (second station) (second asteroid))]
     (if (not= dx 0)
       [(> dx 0) (/ dy dx)]
       (if (not= dy 0)
-        [(> dy 0) :infinity]))))        
+        [(< dy 0) :infinity]))))
 
 (defn detectable-asteroids [station asteroids]
   (count (distinct (keep (partial signed-gradient station) asteroids))))
@@ -122,5 +123,95 @@ Find the best location for a new monitoring station. How many other asteroids ca
 
 (def input (read-lines (str/split (slurp "src/advent_of_code_2019/day10.input") #"\r\n")))
 
+;; (best-station input)
+;; => [13 17]
+
 ;; (detectable-asteroids (best-station input) input)
 ;; => 269
+
+"""
+--- Part Two ---
+Once you give them the coordinates, the Elves quickly deploy an Instant Monitoring Station to the location and discover the worst: there are simply too many asteroids.
+
+The only solution is complete vaporization by giant laser.
+
+Fortunately, in addition to an asteroid scanner, the new monitoring station also comes equipped with a giant rotating laser perfect for vaporizing asteroids. The laser starts by pointing up and always rotates clockwise, vaporizing any asteroid it hits.
+
+If multiple asteroids are exactly in line with the station, the laser only has enough power to vaporize one of them before continuing its rotation. In other words, the same asteroids that can be detected can be vaporized, but if vaporizing one asteroid makes another one detectable, the newly-detected asteroid won't be vaporized until the laser has returned to the same position by rotating a full 360 degrees.
+
+For example, consider the following map, where the asteroid with the new monitoring station (and laser) is marked X:
+
+.#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....X...###..
+..#.#.....#....##
+The first nine asteroids to get vaporized, in order, would be:
+
+.#....###24...#..
+##...##.13#67..9#
+##...#...5.8####.
+..#.....X...###..
+..#.#.....#....##
+Note that some asteroids (the ones behind the asteroids marked 1, 5, and 7) won't have a chance to be vaporized until the next full rotation. The laser continues rotating; the next nine to be vaporized are:
+
+.#....###.....#..
+##...##...#.....#
+##...#......1234.
+..#.....X...5##..
+..#.9.....8....76
+The next nine to be vaporized are then:
+
+.8....###.....#..
+56...9#...#.....#
+34...7...........
+..2.....X....##..
+..1..............
+Finally, the laser completes its first full rotation (1 through 3), a second rotation (4 through 8), and vaporizes the last asteroid (9) partway through its third rotation:
+
+......234.....6..
+......1...5.....7
+.................
+........X....89..
+.................
+In the large example above (the one with the best monitoring station location at 11,13):
+
+The 1st asteroid to be vaporized is at 11,12.
+The 2nd asteroid to be vaporized is at 12,1.
+The 3rd asteroid to be vaporized is at 12,2.
+The 10th asteroid to be vaporized is at 12,8.
+The 20th asteroid to be vaporized is at 16,0.
+The 50th asteroid to be vaporized is at 16,9.
+The 100th asteroid to be vaporized is at 10,16.
+The 199th asteroid to be vaporized is at 9,6.
+The 200th asteroid to be vaporized is at 8,2.
+The 201st asteroid to be vaporized is at 10,9.
+The 299th and final asteroid to be vaporized is at 11,1.
+The Elves are placing bets on which will be the 200th asteroid to be vaporized. Win the bet by determining which asteroid that will be; what do you get if you multiply its X coordinate by 100 and then add its Y coordinate? (For example, 8,2 becomes 802.)
+"""
+
+(defn from-north [station asteroid]
+  (let [dx (- (first asteroid) (first station))
+        dy (- (second station) (second asteroid))
+        bearing (Math/atan2 dx dy)]
+    (if (< bearing 0) (+ (* 2 Math/PI) bearing) bearing)))
+
+(defn update-map [m f] 
+  (reduce-kv (fn [m k v] (assoc m k (f v))) {} m))
+
+(defn norm-squared [origin pt]
+  (let [dx (- (first pt) (first origin))
+        dy (- (second pt) (second origin))]
+  (+ (* dx dx) (* dy dy))))
+
+(defn points-by-angle [origin asteroids]
+  (vec (group-by (partial from-north origin) (remove #(= origin %) asteroids))))
+
+(defn sorted-by-angle [origin asteroids]
+  (map second (sort-by first (points-by-angle origin asteroids))))
+
+(defn explosion-order [origin asteroids]
+  (apply interleave (map (partial sort-by (partial norm-squared origin)) (sorted-by-angle origin asteroids))))
+
+;; (nth (explosion-order [13 17] input) 199)
+;; [6 12]
