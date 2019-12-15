3796adbf587ce54486113251000c539d2142c4ad
Ru James <ruairidh.d.james@gmail.com>
2019-12-15 18:30:28 +0000

    Day 12 - Part 2!
    Thought my cycle search was going wrong at first since the repetitions
    all came back to the very first state. Seems the puzzle designers were
    being forgiving.

--- src/advent_of_code_2019/day12.clj
+++ src/advent_of_code_2019/day12.clj
@@ -229,3 +229,89 @@ What is the total energy in the system after simulating the moons given in your 
                :velocity [0 0 0]}))))
   
 ;; (total-energy (nth (iterate step input) 1000))
+
+"""
+--- Part Two ---
+All this drifting around in space makes you wonder about the nature of the universe. Does history really repeat itself? You're curious whether the moons will ever return to a previous state.
+
+Determine the number of steps that must occur before all of the moons' positions and velocities exactly match a previous point in time.
+
+For example, the first example above takes 2772 steps before they exactly match a previous point in time; it eventually returns to the initial state:
+
+After 0 steps:
+pos=<x= -1, y=  0, z=  2>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  2, y=-10, z= -7>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  4, y= -8, z=  8>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  3, y=  5, z= -1>, vel=<x=  0, y=  0, z=  0>
+
+After 2770 steps:
+pos=<x=  2, y= -1, z=  1>, vel=<x= -3, y=  2, z=  2>
+pos=<x=  3, y= -7, z= -4>, vel=<x=  2, y= -5, z= -6>
+pos=<x=  1, y= -7, z=  5>, vel=<x=  0, y= -3, z=  6>
+pos=<x=  2, y=  2, z=  0>, vel=<x=  1, y=  6, z= -2>
+
+After 2771 steps:
+pos=<x= -1, y=  0, z=  2>, vel=<x= -3, y=  1, z=  1>
+pos=<x=  2, y=-10, z= -7>, vel=<x= -1, y= -3, z= -3>
+pos=<x=  4, y= -8, z=  8>, vel=<x=  3, y= -1, z=  3>
+pos=<x=  3, y=  5, z= -1>, vel=<x=  1, y=  3, z= -1>
+
+After 2772 steps:
+pos=<x= -1, y=  0, z=  2>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  2, y=-10, z= -7>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  4, y= -8, z=  8>, vel=<x=  0, y=  0, z=  0>
+pos=<x=  3, y=  5, z= -1>, vel=<x=  0, y=  0, z=  0>
+Of course, the universe might last for a very long time before repeating. Here's a copy of the second example from above:
+
+<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>
+This set of initial positions takes 4686774924 steps before it repeats a previous state! Clearly, you might need to find a more efficient way to simulate the universe.
+
+How many steps does it take to reach the first state that exactly matches a previous state?
+"""
+
+(defn gravity [axis other-axis]
+  (+- (:position axis) (:position other-axis)))
+
+(defn velocity [axis axes]
+  (->> axes
+       (map (partial gravity axis))
+       (reduce + (:velocity axis))))
+
+(defn update-velocity [axis others]
+  (assoc axis :velocity (velocity axis others)))
+
+(defn update-position [axis]
+  (assoc axis :position (+ (:position axis) (:velocity axis))))
+
+(defn get-axis [moons n]
+  (map #(assoc {}
+               :position (nth (:position %) n)
+               :velocity (nth (:velocity %) n)) moons))
+
+;; (count (set (take 1000000 (iterate step (get-axis input 0)))))
+;; => 286332
+;; ... so the x-axis cycle is no more than 286332 long
+
+;; (count (set (take 1000000 (iterate step (get-axis input 1)))))
+;; => 231614
+;; ... so the y-axis cycle is no more than 231614 long
+
+;; (count (set (take 1000000 (iterate step (get-axis input 2)))))
+;; => 60424
+;; ... so the z-axis cycle is no more than 60424 long
+
+;; Naively assume that the cycle loops back to the first state
+
+(defn hcf [a b]
+  (cond (< a b) (recur (- b a) a)
+        (> a b) (recur (- a b) b)
+        (= a b) a))
+
+(defn lcm [a b]
+  (/ (* a b) (hcf a b)))
+
+;; (lcm (lcm 286332 231614) 60424)
+;; => 500903629351944
